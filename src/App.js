d62403772dbeb6d6812a373408f4e8f55a5553cc
JYOTHSNA1331 <60293594+JYOTHSNA1331@users.noreply.github.com>
2022-12-06 14:09:17 +0530

    Assignment3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FEEDBACK from "./components/feedback";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FEEDBACK />
+    </div>
+  );
+}
+
+export default App;
